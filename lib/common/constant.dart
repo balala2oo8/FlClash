109f8ec1dd18d2b59a36b31e381791da6882f29d
balala <balala.ml2018@gmail.com>
2024-10-20 18:15:05 +0800

    test

--- lib/common/constant.dart
+++ lib/common/constant.dart
@@ -45,7 +45,7 @@ const maxMobileWidth = 600;
 const maxLaptopWidth = 840;
 const geodataLoaderMemconservative = "memconservative";
 const geodataLoaderStandard = "standard";
-const defaultTestUrl = "https://www.gstatic.com/generate_204";
+const defaultTestUrl = "http://www.gstatic.com/generate_204";
 final filter = ImageFilter.blur(
   sigmaX: 5,
   sigmaY: 5,
